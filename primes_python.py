56d34e26291b47b087388d4dac5fc3fd83717550
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-06-28 15:19:31 +0200

    ini

---
+++ primes_python.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def primes(kmax):
+    p = np.empty(1000, dtype=np.int)
+    result = []
+    if kmax > 1000:
+        kmax = 1000
+    k = 0
+    n = 2
+    while k < kmax:
+        i = 0
+        while i < k and n % p[i] != 0:
+            i += 1
+        if i == k:
+            p[k] = n
+            k += 1
+            result.append(n)
+        n += 1
+    return result
